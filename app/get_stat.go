1c55bd52a03d8a34153d5570705feeaf724b9746
h04x <liquid.doctrine@gmail.com>
2023-08-18 21:14:21 +0300

    add tests

--- app/get_stat.go
+++ app/get_stat.go
@@ -1,6 +1,7 @@
 package app
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -12,14 +13,16 @@ const GetStatSql = `select date::text, views, clicks, cost,
 round(coalesce(cost / NULLIF(clicks, 0), 0), 2) as cpc, 
 round(coalesce(cost / NULLIF(views, 0) * 1000, 0), 2) as cpm
 from clicks
-where date between $1 and $2 order by date`
+where date between $1 and $2 
+order by %s`
 
 type GetStatReq struct {
-	From string `binding:"required,datetime=2006-01-02"`
-	To   string `binding:"required,datetime=2006-01-02"`
+	From  string `binding:"required,datetime=2006-01-02"`
+	To    string `binding:"required,datetime=2006-01-02"`
+	Order string `binding:"oneof=Date Views Clicks Cost Cpc Cpm"`
 }
 
-type GetStatResp struct {
+type GetStatRow struct {
 	Date   string
 	Views  uint
 	Clicks uint
@@ -31,24 +34,24 @@ type GetStatResp struct {
 func getStat(db *pgxpool.Pool) gin.HandlerFunc {
 	f := func(c *gin.Context) {
 
-		r := GetStatReq{}
+		r := GetStatReq{Order: "Date"}
 		if err := c.ShouldBindBodyWith(&r, binding.JSON); err != nil {
 			c.AbortWithError(http.StatusBadRequest, err)
 			return
 		}
 
-		rows, _ := db.Query(c, GetStatSql, r.From, r.To)
+		rows, _ := db.Query(c, fmt.Sprintf(GetStatSql, r.Order), r.From, r.To)
 		defer rows.Close()
 
-		stats := []GetStatResp{}
+		stats := []GetStatRow{}
 		for rows.Next() {
-			var showStat GetStatResp
-			if err := rows.Scan(&showStat.Date, &showStat.Views, &showStat.Clicks,
-				&showStat.Cost, &showStat.Cpc, &showStat.Cpm); err != nil {
+			var row GetStatRow
+			if err := rows.Scan(&row.Date, &row.Views, &row.Clicks,
+				&row.Cost, &row.Cpc, &row.Cpm); err != nil {
 				c.AbortWithError(http.StatusInternalServerError, err)
 				return
 			}
-			stats = append(stats, showStat)
+			stats = append(stats, row)
 		}
 
 		if err := rows.Err(); err != nil {
--- main_test.go
+++ main_test.go
@@ -27,6 +27,113 @@ func (rq *RequestHelper) DoRequest(method string, url string, body string) ([]by
 	return responseData, rr.Code
 }
 
+func TestClearStat(t *testing.T) {
+	app, err := app.Testing()
+	assert.Equal(t, err, nil)
+	defer app.Db.Close()
+
+	rh := RequestHelper{
+		t,
+		app.Router,
+	}
+
+	// call clear
+	body, actual_code := rh.DoRequest("DELETE", "/clear_stat", "")
+	assert.Equal(t, []byte{}, body)
+	assert.Equal(t, 200, actual_code)
+
+	// add some row
+	body, actual_code = rh.DoRequest("POST", "/save_stat",
+		`{ "date": "1234-12-12" }`)
+	assert.Equal(t, []byte{}, body)
+	assert.Equal(t, 202, actual_code)
+
+	// make sure that row is there
+	body, actual_code = rh.DoRequest("POST", "/get_stat",
+		`{ "from": "1234-12-12", "to": "1234-12-12" }`)
+	assert.Equal(t, `[{"Date":"1234-12-12","Views":0,"Clicks":0,"Cost":"0","Cpc":0,"Cpm":0}]`, string(body))
+	assert.Equal(t, 200, actual_code)
+
+	// call clear
+	body, actual_code = rh.DoRequest("DELETE", "/clear_stat", "")
+	assert.Equal(t, []byte{}, body)
+	assert.Equal(t, 200, actual_code)
+
+	// expected empty array
+	body, actual_code = rh.DoRequest("POST", "/get_stat",
+		`{ "from": "1234-12-12", "to": "1234-12-12" }`)
+	assert.Equal(t, `[]`, string(body))
+	assert.Equal(t, 200, actual_code)
+}
+
+func TestGetStat(t *testing.T) {
+	app, err := app.Testing()
+	assert.Equal(t, err, nil)
+	defer app.Db.Close()
+
+	rh := RequestHelper{
+		t,
+		app.Router,
+	}
+
+	body, actual_code := rh.DoRequest("DELETE", "/clear_stat", "")
+	assert.Equal(t, []byte{}, body)
+	assert.Equal(t, 200, actual_code)
+
+	input := []struct {
+		requested_json string
+		expected_code  int
+	}{
+		{`{ "date": "1111-11-11", "views": 15, "clicks": 0, "cost": "15" }`, 202},
+		{`{ "date": "2222-12-12", "views": 2, "clicks": 4, "cost": "0" }`, 202},
+		{`{ "date": "2222-12-12", "views": 1, "clicks": 1, "cost": "0.01" }`, 202},
+		{`{ "date": "2222-12-13", "views": 9, "clicks": 9, "cost": "0" }`, 202},
+		{`{ "date": "2222-12-12", "views": 1, "clicks": 1, "cost": "2" }`, 202},
+	}
+
+	for _, row := range input {
+		body, actual_code := rh.DoRequest("POST", "/save_stat", row.requested_json)
+		b := assert.Equal(t, []byte{}, body)
+		c := assert.Equal(t, row.expected_code, actual_code)
+		if !(b && c) {
+			t.Errorf("Failed at: %s", row.requested_json)
+		}
+	}
+
+	test_case_get_stat := []struct {
+		requested_json string
+		expected_code  int
+		expected_resp  string
+	}{
+		{`{ "from": "0000-00-00", "to": "0000-00-00" }`, 400, ""},
+		{`{ "from": "0001-01-01", "to": "0001-13-01" }`, 400, ""},
+		{`{ "from": "0001-01-01", "to": "0001-12-1" }`, 400, ""},
+		{`{ "from": "0001-01-01", "to": "0001-01-01" }`, 200, "[]"},
+		{`{ "from": "0001-01-01", "to": "0001-02-01" }`, 200, "[]"},
+
+		{`{ "from": "2222-12-12", "to": "2222-12-12" }`, 200, `[{"Date":"2222-12-12","Views":4,"Clicks":6,"Cost":"2.01","Cpc":0.34,"Cpm":502.5}]`},
+
+		{`{ "from": "1111-11-11", "to": "2222-12-13" }`, 200,
+			`[{"Date":"1111-11-11","Views":15,"Clicks":0,"Cost":"15","Cpc":0,"Cpm":1000},` +
+				`{"Date":"2222-12-12","Views":4,"Clicks":6,"Cost":"2.01","Cpc":0.34,"Cpm":502.5},` +
+				`{"Date":"2222-12-13","Views":9,"Clicks":9,"Cost":"0","Cpc":0,"Cpm":0}]`},
+
+		{`{ "from": "1111-11-11", "to": "2222-12-13", "order": "Views" }`, 200,
+			`[{"Date":"2222-12-12","Views":4,"Clicks":6,"Cost":"2.01","Cpc":0.34,"Cpm":502.5},` +
+				`{"Date":"2222-12-13","Views":9,"Clicks":9,"Cost":"0","Cpc":0,"Cpm":0},` +
+				`{"Date":"1111-11-11","Views":15,"Clicks":0,"Cost":"15","Cpc":0,"Cpm":1000}]`},
+	}
+
+	for _, row := range test_case_get_stat {
+		body, actual_code := rh.DoRequest("POST", "/get_stat", row.requested_json)
+		b := assert.Equal(t, row.expected_resp, string(body))
+		c := assert.Equal(t, row.expected_code, actual_code)
+		if !(b && c) {
+			t.Errorf("Failed at: %s", row.requested_json)
+		}
+	}
+}
+
 func TestSaveStat(t *testing.T) {
 	app, err := app.Testing()
 	assert.Equal(t, err, nil)
@@ -74,28 +181,5 @@ func TestSaveStat(t *testing.T) {
 		if !(b && c) {
 			t.Errorf("Failed at: %s", row.requested_json)
 		}
-
 	}
-}
-
-func TestClearStat(t *testing.T) {
-	app, err := app.Testing()
-	assert.Equal(t, err, nil)
-	defer app.Db.Close()
-
-	rh := RequestHelper{
-		t,
-		app.Router,
-	}
-
-	body, actual_code := rh.DoRequest("POST", "/save_stat",
-		`{ "date": "2020-10-11" }`)
-	assert.Equal(t, []byte{}, body)
-	assert.Equal(t, 202, actual_code)
-
-	body, actual_code = rh.DoRequest("POST", "/get_stat",
-		`{ "from": "2020-10-11", "to": "2020-10-11" }`)
-	assert.Equal(t, []byte{}, body)
-	assert.Equal(t, 200, actual_code)
-
 }
